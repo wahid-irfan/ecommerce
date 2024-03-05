ecaac871c9edc95d521ff5973323d5639d339835
IRFAN WAHID <139098947+wahid-irfan@users.noreply.github.com>
2024-03-05 19:39:28 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from "./config/db.js";
+import authRoutes from './routes/authRoute.js'
+
+
+//configure env
+dotenv.config();
+
+//databse config
+connectDB()
+
+//rest object
+const app = express()
+
+//middlewares
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+app.use('/api/v1/auth',authRoutes);
+//rest api
+app.get('/',(req,res)=>{
+    res.send("<h1>Welcome to Ecommerce App</h1>");
+        
+    })
+
+//PORT 
+const PORT = process.env.PORT||8080;
+
+//run listen1
+app.listen(PORT,() => {
+    console.log(
+        `Server Running on ${process.env.DEV_MODE}mode on port ${PORT}`);
+});
